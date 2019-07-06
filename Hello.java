1f2f1d8c3057f9ae22cb54eeadfd6c3d5e44148d
Sam Howard <howardsl1@appstate.edu>
2019-07-06 15:02:36 -0400

    Next merge

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Dr. Fenwick");
+        java.util.Scanner keyboard = new java.util.Scanner(System.in);
+        String response = keyboard.nextLine();
+        System.out.println("How are you?");
     }
 }
